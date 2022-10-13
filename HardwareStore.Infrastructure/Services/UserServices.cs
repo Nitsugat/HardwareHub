6c4d4d95eb65a1437a268a267c4b728dd8132359
nitsugatt <granadosagustin404@gmail.com>
2022-10-13 19:51:33 +0100

    + cambios

---
+++ HardwareStore.Infrastructure/Services/UserServices.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationServices.DTOs.DTOSadmins;
+using ApplicationServices.Interfaces.Services;
+
+namespace HardwareHub.Infrastructure.Services
+{
+    public class UserServices : IUserServices
+    {
+        public Task AddNew(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetUserByCuil(int cuil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
